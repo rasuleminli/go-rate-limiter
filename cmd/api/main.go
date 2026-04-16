0f89b2521ef31efc3d5ee35a458663eb69651bc2
rasulnavir <rismailov@protonmail.com>
2026-04-17 02:56:14 +0400

    chore: use a private mux

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,16 +8,19 @@ import (
 )
 
 func main() {
-	limited := http.HandlerFunc(
+	mux := http.NewServeMux()
+
+	limitedHandler := http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "Limited, don't overuse me!")
 		},
 	)
-	http.Handle("/limited", ratelimit.RateLimitMiddleware(limited))
 
-	http.HandleFunc("/unlimited", func(w http.ResponseWriter, _ *http.Request) {
+	mux.Handle("/limited", ratelimit.RateLimitMiddleware(limitedHandler))
+
+	mux.HandleFunc("/unlimited", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "Unlimited! Let's go!")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
