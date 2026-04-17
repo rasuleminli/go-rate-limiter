cb606c78238fa7c823d10440bca1287be4a398bb
rasulnavir <rismailov@protonmail.com>
2026-04-17 04:16:46 +0400

    test(ratelimit): add status test

--- internal/ratelimit/limiter_test.go
+++ internal/ratelimit/limiter_test.go
@@ -1,6 +1,8 @@
 package ratelimit
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -72,4 +74,39 @@ func TestAllow_Concurrent(t *testing.T) {
 	if int(counter.Load()) != int(MaxTokens) {
 		t.Fatalf("Expected %d requests to be made, but instead allowed to make %d", int(MaxTokens), int(counter.Load()))
 	}
+}
+
+func TestRateLimitMiddleware_Returns429(t *testing.T) {
+	testIP := "192.168.1.4"
+	memoryStore = make(map[string]*Client)
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	handlerToTest := RateLimitMiddleware(nextHandler)
+
+	// Exhaust the limit
+	for range int(MaxTokens) {
+		req := httptest.NewRequest(http.MethodGet, "/limited", nil)
+		req.RemoteAddr = testIP + ":12345" // need the port because getIP splits it
+		recorder := httptest.NewRecorder()
+
+		handlerToTest.ServeHTTP(recorder, req)
+
+		if recorder.Code != http.StatusOK {
+			t.Fatalf("Expected status OK, but got %d", recorder.Code)
+		}
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/limited", nil)
+	req.RemoteAddr = testIP + ":12345"
+	recorder := httptest.NewRecorder()
+
+	handlerToTest.ServeHTTP(recorder, req)
+
+	if recorder.Code != http.StatusTooManyRequests {
+		t.Fatalf("Expected status 429 (Too Many Requests), but got %d", recorder.Code)
+	}
+
 }
